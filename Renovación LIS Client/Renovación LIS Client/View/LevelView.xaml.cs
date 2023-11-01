94bb62865dcd8689d25a6b78fc78e956744b4de3
Katherinebam <kathy161002@gmail.com>
2023-10-31 23:26:13 -0600

    Avance lobby personal

---
+++ Renovación LIS Client/Renovación LIS Client/View/LevelView.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Renovación_LIS_Client.View
+{
+    /// <summary>
+    /// Lógica de interacción para LevelView.xaml
+    /// </summary>
+    public partial class LevelView : Page
+    {
+        public LevelView()
+        {
+            InitializeComponent();
+        }
+    }
+}
